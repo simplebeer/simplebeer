70866dbae047bbb0185ee31250c395c6da3d174c
Brian Pattison <brian@brianpattison.com>
2015-03-28 11:52:05 -0500

    Add content for landing page

--- clock.rb
+++ clock.rb
@@ -3,22 +3,4 @@ require "./config/boot"
 require "./config/environment"
 
 module Clockwork
-  every(1.day, "InvoiceChargeWorker") do
-    Invoice.past_due.pluck(:id).each do |invoice_id|
-      InvoiceChargeWorker.perform_async(invoice_id)
-    end
-  end
-
-  every(1.hour, "OmniAuthTokenRefreshWorker") do
-    # TODO: Limit to expired tokens. Need to look up how to query #auth_data jsonb column.
-    OmniAuthProvider.where.not(name: "facebook").pluck(:id).each do |provider_id|
-      OmniAuthTokenRefreshWorker.perform_async(provider_id)
-    end
-  end
-
-  every(1.day, "SubscriptionUpdateWorker") do
-    Subscription.where("current_period_end <= ?", Time.now).pluck(:id).each do |subscription_id|
-      SubscriptionUpdateWorker.perform_async(subscription_id)
-    end
-  end
 end
